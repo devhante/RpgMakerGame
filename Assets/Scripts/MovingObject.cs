3530e07edceb09554a833b81381c1ca454b0b5d7
Kyungwook Cho <ironside2001@naver.com>
2020-02-15 17:50:35 +0900

    [Add] 캐릭터 이동 구현

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    private Vector3 vector;
+
+    public float speed;
+    public float runSpeed;
+    private float applyRunSpeed;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool isRunning = false;
+    private bool canMove = true;
+
+    private IEnumerator MoveCoroutine()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            applyRunSpeed = runSpeed;
+            isRunning = true;
+        }
+        else
+        {
+            applyRunSpeed = 0;
+            isRunning = false;
+        }
+
+        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+        while (currentWalkCount < walkCount)
+        {
+            if (vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            }
+            else if (vector.y != 0)
+            {
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+
+            if (isRunning)
+            {
+                currentWalkCount++;
+            }
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        currentWalkCount = 0;
+        canMove = true;
+    }
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            if ((Input.GetAxisRaw("Horizontal") != 0) || (Input.GetAxisRaw("Vertical") != 0))
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
+        }
+    }
+}
