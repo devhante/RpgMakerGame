4b1060c1194c6103f28d8f210eeb52a44404b8ea
Kyungwook Cho <ironside2001@naver.com>
2020-02-29 09:30:56 +0900

    [Add] 카메라 움직임, 맵 제작

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject target;
+    public float moveSpeed;
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        targetPosition.Set(target.transform.position.x, target.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+}
